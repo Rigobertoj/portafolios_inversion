ac5367971ba22c2a487b6da88de5288ec0903a78
Rigoberto Martinez <donojaci@gmail.com>
2026-03-17 11:09:42 -0600

    Implementacion: src/asset_allocation/__init__.py | Incorportamos todos los modulos de asset_allocation al init para poder importarlos mas facilmente

--- src/asset_allocation/__init__.py
+++ src/asset_allocation/__init__.py
@@ -1,17 +1,39 @@
-from .PortfolioElementaryAnalysis import PortfolioElementaryAnalysis
 from .PortfolioElementaryMetrics import PortfolioElementaryMetrics
+
 from .PortfolioOptimization import (
+    MinimumVarianceConfig,
     OptimizationConfig,
     OptimizationResult,
     PortfolioOptimization,
 )
 
+from .PortfolioPostModernMetrics import PortfolioPostModernMetrics
+
+from .PortfolioOptimizationPostModern import (
+    MaximumOmegaConfig,
+    MinimumSemivarianceConfig,
+    PostModernOptimizationConfig,
+    PostModernOptimizationResult,
+    PortfolioOptimizationPostMordern,
+    PortfolioOptimizationPostModern,
+)
+
+
+from .PortfolioElementaryAnalysis import PortfolioElementaryAnalysis
+
 
 __all__ = [
-    "AssetsResearch",
     "PortfolioElementaryAnalysis",
     "PortfolioElementaryMetrics",
+    "MinimumVarianceConfig",
     "OptimizationConfig",
     "OptimizationResult",
     "PortfolioOptimization",
+    "PostModernOptimizationConfig",
+    "MinimumSemivarianceConfig",
+    "MaximumOmegaConfig",
+    "PostModernOptimizationResult",
+    "PortfolioOptimizationPostMordern",
+    "PortfolioOptimizationPostModern",
+    "PortfolioPostModernMetrics",
 ]
