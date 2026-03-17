69c1c38e939b5b16b52efad60b2024ed333dfc1e
Rigoberto Martinez <donojaci@gmail.com>
2026-03-17 10:50:42 -0600

    implementacion: src/security_selection/__init__.py | grege la pieza de codigo que nos permite importar el modulo de CorrelationPortfolioSelector al init e importarlo mas facilmente

--- src/security_selection/__init__.py
+++ src/security_selection/__init__.py
@@ -1,6 +1,8 @@
 from .AssetsResearch import AssetsResearch
+from .CorrelationPortfolioSelector import CorrelationPortfolioSelector
 
 
 __all__ = [
     "AssetsResearch",
+    "CorrelationPortfolioSelector",
 ]
