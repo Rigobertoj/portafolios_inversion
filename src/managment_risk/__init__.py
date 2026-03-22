2202c1485f5ad0a277054aef02769ad90270cb96
Rigoberto Martinez <donojaci@gmail.com>
2026-03-22 15:03:04 -0600

    Implementacion: src/managment_risk/__init__.py | Agregando al init las clases de backtesting

---
+++ src/managment_risk/__init__.py
@@ -0,0 +1,23 @@
+from .BackTestingStrategy import (
+    AllocationStrategy,
+    BacktestConfig,
+    BacktestResult,
+    BacktestStrategyResult,
+    Backtester,
+    BackTestingStrategy,
+    MeanVarianceStrategy,
+    PostModernStrategy,
+    StrategyAllocation,
+)
+
+__all__ = [
+    "AllocationStrategy",
+    "BacktestConfig",
+    "BacktestResult",
+    "BacktestStrategyResult",
+    "Backtester",
+    "BackTestingStrategy",
+    "MeanVarianceStrategy",
+    "PostModernStrategy",
+    "StrategyAllocation",
+]
